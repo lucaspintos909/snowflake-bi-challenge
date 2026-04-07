fe30328df3beb087a679490e582c11fcf62afca9
lucaspintos909 <lucaspintos909@gmail.com>
2026-04-07 10:59:31 -0300

    feat(utils): Funciones para manejar y obtener las sesiones

---
+++ utils/session.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection_params() -> dict:
+    return {
+        "account": os.environ["SNOWFLAKE_ACCOUNT"],
+        "user": os.environ["SNOWFLAKE_USER"],
+        "password": os.environ["SNOWFLAKE_PASSWORD"],
+        "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
+        "role": os.environ["SNOWFLAKE_ROLE"],
+        "database": "CEIBAL_DB",
+        "schema": "RAW",
+    }
+
+
+def get_session():
+    from snowflake.snowpark import Session
+    return Session.builder.configs(get_connection_params()).create()
